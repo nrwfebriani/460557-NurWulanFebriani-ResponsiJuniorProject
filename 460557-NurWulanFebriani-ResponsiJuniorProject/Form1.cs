44744a7781198d705727b013a9e59fd4063edf0f
Nur Wulan Febriani <Nur Wulan Febriani@DESKTOP-E7G9BI1>
2022-11-29 12:42:20 +0700

    Add project files.

---
+++ 460557-NurWulanFebriani-ResponsiJuniorProject/Form1.cs
@@ -0,0 +1,10 @@
+namespace _460557_NurWulanFebriani_ResponsiJuniorProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
